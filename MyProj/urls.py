9f33725b43161c8fad2b668e37159c810829e175
RicardoHonda <ricardothonda@usp.br>
2022-09-29 16:48:41 -0300

    ✨ Update base pages
    Co-authored-by: Henrique D'Amaral Matheus <DamaralHenrique@users.noreply.github.com>
    Co-authored-by: Edu-Hiroshi <Edu-Hiroshi@users.noreply.github.com>

--- MyProj/urls.py
+++ MyProj/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from book import views
+from flight import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('FIRST', views.bookviews),
+    path('', views.flightviews),
 ]
