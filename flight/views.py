3c3e0061c4a65d2cce2da564b43bbc6b722b22af
RicardoHonda <ricardothonda@usp.br>
2022-10-13 17:24:54 -0300

    🚧 Update interfaces

--- flight/views.py
+++ flight/views.py
@@ -12,3 +12,24 @@ def telaGerarRelatorioViews(request):
 
 def telaPreviewRelatorioViews(request):
     return render(request, "preview_relatorio.html")
+
+def telaPainelMonitoramentoViews(request):
+    return render(request, "monitoramento_painel.html")
+
+def telaMonitoramentoViews(request):
+    return render(request, "monitoramento_voo.html")
+
+def telaAtualizarMonitoramentoViews(request):
+    return render(request, "monitoramento_atualizacao.html")
+
+def telaListaVoosViews(request):
+    return render(request, "lista_voos.html")
+
+def telaCreateVooViews(request):
+    return render(request, "c_voo.html")
+
+def telaUpdateVooViews(request):
+    return render(request, "u_voo.html")
+    
+def telaReadDeleteVooViews(request):
+    return render(request, "rd_voo.html")
